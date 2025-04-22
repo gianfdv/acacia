2fc860a102bd133013ca9f57f5fc9c09f63e51f3
GianfrancoDV <gdarsie@hotmail.com>
2025-04-22 17:30:47 -0600

    Elopement image

--- src/components/ElopementSection.jsx
+++ src/components/ElopementSection.jsx
@@ -16,7 +16,7 @@ const ElopementsSection = () => {
     <div className="p-2 w-full lg:w-2/5 flex justify-center lg:justify-start items-center">
       <img 
         className="h-[400px] rounded-lg" 
-        src="https://res.cloudinary.com/dxvkewdcn/image/upload/q_auto,f_auto,w_1000/albums/kelsey-gerald/image13.jpg" 
+        src="https://res.cloudinary.com/dxvkewdcn/image/upload/q_auto,f_auto,w_1000/albums/kelsey-gerald/image29.jpg" 
         alt="Wedding Details" 
       />
     </div>
