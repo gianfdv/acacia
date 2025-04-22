f310bba0d30c439799831d7aadf97e965a73a084
GianfrancoDV <gdarsie@hotmail.com>
2025-04-22 12:18:33 -0600

    Pictures changes 6

--- src/components/OptionsSection.jsx
+++ src/components/OptionsSection.jsx
@@ -19,7 +19,7 @@ const OptionsSection = () => {
         </Link>
         <Link to="/contact-us" className="w-full sm:w-1/2 lg:w-1/3 p-2">
             <div className="p-6 rounded-xl flex flex-col sm:flex-row items-center text-center">
-                <img className="sm:w-1/2 h-36 object-cover rounded-lg" src={detailsImg}/>
+                <img className="sm:w-1/2 h-36 object-cover rounded-lg" src="https://res.cloudinary.com/dxvkewdcn/image/upload/q_auto,f_auto,w_1000/albums/samantha-michael/image2.jpg"/>
                 <h1 className="sm:w-1/2 p-2 text-3xl hover:text-[#0b3425]">Contact us</h1>
             </div>
         </Link>
