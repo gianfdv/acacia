27878ac74c7b118d5b156c29368dd21a069a939a
GianfrancoDV <gdarsie@hotmail.com>
2025-04-22 11:09:34 -0600

    Hero changes 4

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
             Costa Rica!
         </h1>
         <div className="flex mt-0 justify-center w-full h-[480px]">
-            <video autoPlay loop muted className="rounded-lg w-full h-full object-cover mx-2 my-2">
+        <video autoPlay loop muted playsInline className="rounded-lg w-full h-full object-cover mx-2 my-2">
                 <source src={video} type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
